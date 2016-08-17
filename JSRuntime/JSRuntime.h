94724a3d7c6d543c03e51ef5d0b4819c2af69377
程巍巍 <Littocats@gmail.com>
2016-08-17 15:34:32 +0800

    Initial Commit

---
+++ JSRuntime/JSRuntime.h
@@ -0,0 +1,13 @@
+//
+//  JSRuntime.h
+//  JSRuntime
+//
+//  Created by 程巍巍 on 8/17/16.
+//  Copyright © 2016 程巍巍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSRuntime : NSObject
+
+@end
---
+++ JSRuntime/JSRuntime.m
@@ -0,0 +1,13 @@
+//
+//  JSRuntime.m
+//  JSRuntime
+//
+//  Created by 程巍巍 on 8/17/16.
+//  Copyright © 2016 程巍巍. All rights reserved.
+//
+
+#import "JSRuntime.h"
+
+@implementation JSRuntime
+
+@end
